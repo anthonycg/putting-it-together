0389c00bb4c608d075298ceece83aa84aa78fae5
Anthony <anthonygibson24@gmail.com>
2022-06-30 21:28:13 -0500

    utilized classes to complete project.

--- src/App.js
+++ src/App.js
@@ -1,22 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
+import PersonCard from './components/PersonCard';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <div className="App">
+      <PersonCard
+        lastName={"Gibson"}
+        firstName={"Anthony"}
+        age={27}
+        hair={"black"}
+      />
+      <PersonCard
+        lastName={"Gibson"}
+        firstName={"Arkeem"}
+        age={29}
+        hair={"black"}
+      />
+      <PersonCard 
+      lastName={"Woodson"} 
+      firstName={"Arkeem"} 
+      age={27} 
+      hair={"black"} />
+      <PersonCard
+        lastName={"Carmichael"}
+        firstName={"Frank"}
+        age={71}
+        hair={"gray"}
+      />
+    </div>
       </header>
     </div>
   );
